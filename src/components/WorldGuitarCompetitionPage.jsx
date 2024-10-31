ee808f0b1bffb9ebd6aabc0ea967c2ced304e2aa
Victor Hernandez <victor.hernandez25@upr.edu>
2024-10-31 08:42:59 -0400

    New form

--- src/components/WorldGuitarCompetitionPage.jsx
+++ src/components/WorldGuitarCompetitionPage.jsx
@@ -249,11 +249,12 @@ const WorldGuitarCompetitionPage = () => {
             {/* Add form */}
             <form 
               name="score-download" 
-              method="POST" 
-              data-netlify="true"
+              method="POST"
+              netlify
               className="max-w-2xl mx-auto mb-8 bg-black/30 p-6 rounded-lg"
             >
               <input type="hidden" name="form-name" value="score-download" />
+              <input type="hidden" name="bot-field" />
               
               <h3 className="text-2xl font-bold text-[#498FC6] mb-6">Please fill out this form to access the downloads</h3>
               <div className="space-y-4">
